a412257b725b2d5ad31a3484fab90206f859962f
Frantkich <francoisguern78@gmail.com>
2024-01-02 22:06:36 +0100

    yeah i guess

--- app/tools/config.py
+++ app/tools/config.py
@@ -7,7 +7,7 @@ class Config(object):
     TEMPLATE_FOLDER = os.getcwd() + "/app/templates"
     STATIC_FOLDER = os.getcwd() + "/app/static"
     SQL_USERNAME = "syncordian"
-    SQL_HOST = "192.168.1.190"
+    SQL_HOST = "frantkich.fr"
     SQL_DATABASE = "texam"
     SQLALCHEMY_DATABASE_URI = (f"mysql://{SQL_USERNAME}@{SQL_HOST}:3306/{SQL_DATABASE}")
 
