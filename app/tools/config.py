7592c42196bee4fca6d1bd13e98eb0ca4068f0e4
Guern François <37771409+Frantkich@users.noreply.github.com>
2025-06-18 13:58:36 +0200

    Update config.py

--- app/tools/config.py
+++ app/tools/config.py
@@ -4,13 +4,13 @@ import os
 class Config(object):
     PROJECT_NAME = "texam"
     SQL_DATABASE = PROJECT_NAME
-    SECRET = "tlcsucks"
-    SECRET_KEY = "El_Christ_bagnicong_de_la_keifuern"
-    TLCEXAM_URL = "https://tlcexams.temenos.com/partner"
+    SECRET =
+    SECRET_KEY =
+    TLCEXAM_URL =
     TEMPLATE_FOLDER = os.getcwd() + "/app/templates"
     STATIC_FOLDER = os.getcwd() + "/app/static"
-    SQL_USERNAME = "syncordian"
-    SQL_HOST = "frantkich.fr"
+    SQL_USERNAME = 
+    SQL_HOST = 
     SQLALCHEMY_DATABASE_URI = (f"mysql://{SQL_USERNAME}@{SQL_HOST}:3306/{SQL_DATABASE}")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
